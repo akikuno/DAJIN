21c4044b87d12f19387fac79ec1a05acfc9dfad3
Akihiro Kuno <akihiro.kuno@gmail.com>
2020-02-13 12:09:12 +0900

    minimize code

--- src/test_anomaly_classification.sh
+++ src/test_anomaly_classification.sh
@@ -1,92 +1,103 @@
 #!/bin/sh
 
+# # =====================================================
+# # Generate test data
+# # =====================================================
 # =====================================================
-# Generate test data
+## Select barcode with Abnormal(target_deletion) > 80%
 # =====================================================
-printf "
-02bbb775\t0
-042c068f\t0
-0324be19\t0
-07bececa\t1
-0df976dc\t1
-05f9d0e3\t1
-0f838c72\t2
-0d29b0af\t2
-07fa3b6f\t2
-" |
-sort |
-grep -v "^$" \
-> tmp_testdata
-
-# ========================================
-barcode=barcode28
-barcode=barcode12
-# ========================================
-
-cat .tmp_/anomaly_classification_revised.txt |
-grep ${barcode} | grep target_deletion |
-cut -f 2 |
-awk '{gsub("-.*","",$1); print}' |
-sort > tmpid
-
-samtools view bam/${barcode}.bam |
-awk '{gsub("-.*","",$1); print}' |
-sort |
-join tmpid - > tmp
-
-cat tmp | #grep dd3a08ee |
-awk '$2==0 || $2==16' |
-sed "s/ /\t/g" |
-awk '{print $1,$4,$(NF-1)}' |
-sed -e "s/cs:Z:://g" \
--e "s/~[a-z]*/ /g" |
-awk '{gsub("[a-z].*","",$NF); print}' |
-sed -e "s/:/ /g" -e "s/\*[a-z][a-z]/+1/g" \
-    -e "s/+/ /g" -e "s/-/ -/g" |
-awk '{for(i=3;i<=NF-1;i++) if($i~"-") $i="-"length($i)-1;
-    else if($i ~ /[a-z]/) $i=length($i);
-    print}' |
-awk '{sum=1; for(i=3;i<=NF-1;i++) sum+=$i
-    print $1,$2+sum,$2+sum+$NF, $NF}' \
-> tmp_testdata2
-
-# =============================
-rm tmp*
-for cl in $(cat hoge.txt | awk '{print $NF}' | sort | uniq); do
-    echo $cl
-    cat hoge.txt |
-    awk -v cl=${cl} '$NF==cl' |
-    cut -f 1 |
-    sort \
-    > tmp_${cl}
+true > .tmp_/barcodelist
+for barcode in $(cat .tmp_/anomaly_classification_revised.txt | cut -f 1 | sort -u); do
+    cat .tmp_/anomaly_classification_revised.txt |
+    awk -v id=${barcode} '$1 == id' |
+    cut -f 3 |
+    sort |
+    uniq -c |
+    awk '{if($2 ~ "target_deletion") td=$1;
+        sum+=$1}
+        END{print int(td/sum*100)}' |
+    xargs -I @ [ @ -gt 80 ] &&
+    printf "${barcode}\n" \
+    >> .tmp_/barcodelist
 done
+# ========================================
+# ========================================
+output_csv="abnormal_class_info.csv"
+output_dir="bam/abnormal_class"
+#
+printf "barcodeID,cluster,number of reads\n" \
+> ${output_dir}/${output_csv}
+mkdir -p ${output_dir}
+#
+for barcode in $(cat .tmp_/barcodelist); do
+    cat .tmp_/anomaly_classification_revised.txt |
+    awk -v id=${barcode} '$1 == id' |
+    grep target_deletion |
+    cut -f 2 |
+    awk '{gsub("-.*","",$1); print}' |
+    sort > .tmp_/tmp_id
+    #
+    # Extract ID, cuttind sites and cutting size
+    #
+    samtools view bam/${barcode}.bam |
+    awk '{gsub("-.*","",$1); print}' |
+    sort -k 1,1 |
+    join .tmp_/tmp_id - 2>/dev/null |
+    awk '$2==0 || $2==16' |
+    sed "s/ /\t/g" |
+    awk '{print $1,$4,$(NF-1)}' |
+    sed -e "s/cs:Z:://g" \
+    -e "s/~[a-z]*/ /g" |
+    awk '{gsub("[a-z].*","",$NF); print}' |
+    sed -e "s/:/ /g" -e "s/\*[a-z][a-z]/+1/g" \
+        -e "s/+/ /g" -e "s/-/ -/g" |
+    awk '{for(i=3;i<=NF-1;i++) if($i~"-") $i="-"length($i)-1;
+        else if($i ~ /[a-z]/) $i=length($i);
+        print}' |
+    awk '{sum=1; for(i=3;i<=NF-1;i++) sum+=$i
+        print $1,$2+sum,$2+sum+$NF, $NF}' \
+    > .tmp_/tmp_anomaly_classification
+    # =============================
+    python ./DAJIN/src/test_anomaly_classification.py
+    # =============================
+    for cl in $(cat .tmp_/tmp_anomaly_label | awk '{print $NF}' | sort | uniq); do
+        #
+        cat .tmp_/tmp_anomaly_label |
+        awk -v cl=${cl} '$NF==cl' |
+        cut -f 1 |
+        sort \
+        > .tmp_/tmp_
+        #
+        # if [ ${cl} -eq 0 ]; then
+        #     cl="unclassified"
+        # else
+        #     cl="cluster"${cl}
+        # fi
+        cl="cluster"${cl}
+#        [ ${cl} -eq 0 ] && cl="unclassified"
+#        [ ${cl} -ne 0 ] && cl="cluster"${cl}
+        #
+        samtools view -h bam/${barcode}.bam |
+        grep "^@" >.tmp_/tmp_header
+        #
+        samtools view bam/${barcode}.bam |
+        awk '$2==0 || $2==16' |
+        awk '{gsub("-.*","",$1); print}' |
+        sort |
+        join .tmp_/tmp_ - 2>/dev/null |
+        head -n 100 |
+        sed "s/ /\t/g" \
+        >> .tmp_/tmp_header
+        #
+        samtools sort .tmp_/tmp_header > ${output_dir}/${barcode}_${cl}.bam
+        samtools index ${output_dir}/${barcode}_${cl}.bam
+        #
+        cat .tmp_/tmp_ |
+        wc -l |
+        sed "s/^/${barcode},${cl},/g" \
+        >> ${output_dir}/${output_csv}
+    done
+done
+rm .tmp_/tmp_*
+exit 0
 
-samtools view -h bam/${barcode}.bam |
-grep "^@" > tmp_header
-
-samtools view bam/${barcode}.bam |
-awk '{gsub("\-.*","",$1); print}' |
-sort |
-join tmp_-1 - |
-head -n 100 |
-sed "s/ /\t/g" \
->> tmp_header
-
-samtools sort tmp_header > tmp.bam
-samtools index tmp.bam
-
-
-# --------------------
-barcode=barcode12
-samtools view -h bam/${barcode}.bam |
-grep "^@" > tmp_header
-
-samtools view bam/${barcode}.bam |
-awk '{gsub("\-.*","",$1); print}' |
-sort |
-join tmp_testdata - |
-sed "s/ /\t/g" |
-cut -f 1,3- \
->> tmp_header
-samtools sort tmp_header > tmp.bam
-samtools index tmp.bam
