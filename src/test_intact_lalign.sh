eaa8989cd377ffa785f9ec3fe855fe942b39f8d2
Akihiro Kuno <akihiro.kuno@gmail.com>
2020-02-21 17:43:33 +0900

    tmp_${file_name}

--- src/test_intact_lalign.sh
+++ src/test_intact_lalign.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 # TGATGCTCGGC ref >2655 zczc >4703 zfzw
 # TGATGCTCGGT mut >249 jo >5147 zgqy
+# grep ">8546" .tmp_/split/*
+# grep ">7737" .tmp_/split/*
+
 mut_length=$(cat "${1}" | tail -n 1 | awk '{print length($0)}')
-#lalign36 -m 3 .tmp_/mutation.fa .tmp_/split/split_zgqy -b # |
+file_name=$(echo "${2}" | sed "s#.*/##g")
+# lalign36 -m 3 .tmp_/mutation.fa .tmp_/split/split_zevq |
 lalign36 -m 3 "${1}" "${2}" |
 sed -n "/identity/,/^$/p" |
 grep -v "^$" |
@@ -18,26 +22,27 @@ sed -e "s/FUGA/\n/g" -e "s/>/ >/g" |
 sed -e "s/% similar).*overlap (/ /g" -e "s/-/ /" |
 grep -v "^$" |
 sort -n |
-tail -n 1 \
-> .tmp_/split/tmp
+tail -n 1 |
+awk -v mut_len=${mut_length} 'length($NF)>mut_len-10' \
+> .tmp_/split/tmp_${file_name}
 #
 # Reverse complement
 #
- [ ! -s .tmp_/split/tmp ] && exit 1
+ [ ! -s .tmp_/split/tmp_${file_name} ] && exit 1
 
-start=$(cat .tmp_/split/tmp | cut -d " " -f 2)
-end=$(cat .tmp_/split/tmp | cut -d " " -f 3)
-seq=$(cat .tmp_/split/tmp | awk '{print $NF}')
-query=".tmp_/split/tmp"
+start=$(cat .tmp_/split/tmp_${file_name} | cut -d " " -f 2)
+end=$(cat .tmp_/split/tmp_${file_name} | cut -d " " -f 3)
+seq=$(cat .tmp_/split/tmp_${file_name} | awk '{print $NF}')
+query=".tmp_/split/tmp_${file_name}"
 
 if [ "$start" -gt "$end" ]; then
-    cat .tmp_/split/tmp | awk '{print $NF}' > .tmp_/split/tmp_seq
-    revseq=$(./DAJIN/src/revcomp.sh .tmp_/split/tmp_seq)
-    #revseq=$(./revcomp.sh .tmp_/split/tmp_seq)
-    cat .tmp_/split/tmp | sed "s/${seq}/${revseq}/g" |
+    cat .tmp_/split/tmp_${file_name} | awk '{print $NF}' \
+    > .tmp_/split/tmp_${file_name}_seq
+    revseq=$(./DAJIN/src/revcomp.sh .tmp_/split/tmp_${file_name}_seq)
+    cat .tmp_/split/tmp_${file_name} | sed "s/${seq}/${revseq}/g" |
     awk -v s=${start} -v e=${end} '{$2=e;$3=s; print}' \
-    > .tmp_/split/tmp_rev
-    query=".tmp_/split/tmp_rev"
+    > .tmp_/split/tmp_${file_name}_rev
+    query=".tmp_/split/tmp_${file_name}_rev"
 fi
 #
 cat ${query} |
@@ -53,7 +58,7 @@ awk -v mut_len=${mut_length} '{
     for(i=1;i<=int(start);i++) s_seq=s_seq"-"
     for(i=1;i<=int(end);i++) e_seq=e_seq"-"
     #print s_seq, e_seq
-    print $6"_"$1"\n"s_seq,$NF,e_seq
+    print $1,$6,s_seq""$NF""e_seq
 }'
 # Extract 5(50-6+1) and 12(50-38)
 # awk -v mut_len=${mut_length} '{
