e986b6cf46a03675cd974b486bb3f7f077bf9ae6
Akihiro Kuno <akihiro.kuno@gmail.com>
2020-03-04 12:09:15 +0900

    Stx2 #2 real sample

--- src/test_clustering.sh
+++ src/test_clustering.sh
@@ -1,11 +1,33 @@
 #!/bin/sh
-cat data_for_ml/sequence_MIDS.txt |
-grep "wt" |
-cut -f 1,3 > test_labels.txt
+# cat data_for_ml/sequence_MIDS.txt |
+# grep "wt" |
+# cut -f 1,3 > test_labels.txt
 
-cat data_for_ml/sequence_MIDS.txt |
-grep "wt" |
-cut -f 2 |
+# cat data_for_ml/sequence_MIDS.txt |
+# grep "wt" |
+# cut -f 2 |
+# awk -F "" '{
+#     for(i=1; i<=NF; i++){
+#         if($i=="I" && $(i+1)!="I") $(i+1)="@"
+#         }
+#     print $0}' |
+# sed -e "s/I//g" -e "s/ //g" \
+# > tmp_seq
+
+# paste test_labels.txt tmp_seq \
+# > test_tmp_seq
+
+
+zcat data_for_ml/sequence_MIDS.txt.gz |
+grep "barcode02" |
+join -1 3 -2 2 - .tmp_/sorted_prediction_result |
+cut -d " " -f 1,2 |
+sed "s/ /\t/g" > test_labels.txt
+
+zcat data_for_ml/sequence_MIDS.txt.gz |
+grep "barcode02" |
+join -1 3 -2 2 - .tmp_/sorted_prediction_result |
+cut -d " " -f 3 |
 awk -F "" '{
     for(i=1; i<=NF; i++){
         if($i=="I" && $(i+1)!="I") $(i+1)="@"
@@ -13,10 +35,6 @@ awk -F "" '{
     print $0}' |
 sed -e "s/I//g" -e "s/ //g" \
 > tmp_seq
-
-# paste test_labels.txt tmp_seq \
-# > test_tmp_seq
-
 
 input="tmp_seq"
 output="test.csv"
@@ -60,20 +78,35 @@ Rscript clustering.R
 # ./DAJIN/src/igvjs.sh ${genome:-mm10} ${threads:-1}
 
 # Separate BAM files
-bam="wt_merged.bam"
+barcode=barcode02
 cat test_result.txt | cut -f 2 | sort | uniq -c
 #
-rm tmp*.bam* 2>/dev/null
+rm ${barcode}*.bam* 2>/dev/null
 for i in $(cat test_result.txt | cut -f 2 | sort -u);do
     cat test_result.txt | grep ${i}$ | cut -f 1 | sort > tmp_id
-    samtools view -h bam/${bam} | grep "^@" > tmp_header 
-    samtools view bam/${bam} |
+    samtools view -h bam/${barcode}.bam | grep "^@" > tmp_header 
+    #
+    samtools view bam/${barcode}.bam |
     sort |
     join - tmp_id |
     sed "s/ /\t/g" |
     head -n 100 \
     >> tmp_header
-    samtools sort tmp_header > tmp${i}.bam
-    samtools index tmp${i}.bam
+    #
+    samtools sort tmp_header > ${barcode}_cl${i}.bam
+    samtools index ${barcode}_cl${i}.bam
 done
+
+samtools view -h bam/${barcode}.bam | grep "^@" > tmp_header 
+#
+samtools view bam/${barcode}.bam |
+sort |
+join - tmp_id |
+sed "s/ /\t/g" |
+head -n 100 \
+>> tmp_header
+#
+samtools sort tmp_header > ${barcode}_all.bam
+samtools index ${barcode}_all.bam
+
 rm tmp_*
