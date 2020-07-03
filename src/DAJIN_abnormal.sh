07d043ae90cac0407f20d03d6ddc6445145bd701
akikuno <akihiro.kuno@gmail.com>
2020-07-03 10:02:44 +0900

    prepare ACGT conv

---
+++ src/DAJIN_abnormal.sh
@@ -0,0 +1,456 @@
+#!/bin/bash
+
+################################################################################
+#! Initialize shell environment
+################################################################################
+
+set -u
+umask 0022
+export LC_ALL=C
+export UNIX_STD=2003  # to make HP-UX conform to POSIX
+
+
+################################################################################
+#! I/O settings
+################################################################################
+#===========================================================
+#? Input
+#===========================================================
+
+design=DAJIN/misc/abnormal_detection/cables2_anomaldetection.fa
+ont_cont=barcode21
+ont_dir=fastq
+
+#===========================================================
+#? Define threads
+#===========================================================
+
+expr "${threads}" + 1 >/dev/null 2>&1
+if [ $? -lt 2 ]; then
+    :
+else
+    unset threads
+    # Linux and similar...
+    threads=$(getconf _NPROCESSORS_ONLN 2>/dev/null | awk '{print int($0/1.5+0.5)}')
+    # FreeBSD and similar...
+    [ -z "$threads" ] && threads=$(getconf NPROCESSORS_ONLN | awk '{print int($0/1.5)+0.5}')
+    # Solaris and similar...
+    [ -z "$threads" ] && threads=$(ksh93 -c 'getconf NPROCESSORS_ONLN' | awk '{print int($0/1.5+0.5)}')
+    # Give up...
+    [ -z "$threads" ] && threads=1
+fi
+
+################################################################################
+#! Setting Conda environment
+################################################################################
+
+#===========================================================
+#? DAJIN_nanosim
+#===========================================================
+set +u
+type conda > /dev/null 2>&1 || error_exit 'Command "conda" not found'
+
+CONDA_BASE=$(conda info --base)
+source "${CONDA_BASE}/etc/profile.d/conda.sh"
+
+if [ "$(conda info -e | grep -c DAJIN_nanosim)" -eq 0 ]; then
+    conda config --add channels defaults
+    conda config --add channels bioconda
+    conda config --add channels conda-forge
+    conda create -y -n DAJIN_nanosim python=3.6
+    conda install -y -n DAJIN_nanosim --file ./DAJIN/utils/NanoSim/requirements.txt
+    conda install -y -n DAJIN_nanosim minimap2
+fi
+
+#===========================================================
+#? DAJIN
+#===========================================================
+
+if [ "$(conda info -e | cut -d " " -f 1 | grep -c DAJIN$)" -eq 0 ]; then
+    conda config --add channels defaults
+    conda config --add channels bioconda
+    conda config --add channels conda-forge
+    conda create -y -n DAJIN python=3.6 \
+        anaconda nodejs wget \
+        tensorflow tensorflow-gpu swifter \
+        samtools minimap2 \
+        r-essentials r-base
+fi
+set -u
+
+#===========================================================
+#? Required software
+#===========================================================
+set +u
+conda activate DAJIN
+set -u
+
+type gzip > /dev/null 2>&1 || error_exit 'Command "gzip" not found'
+type wget > /dev/null 2>&1 || error_exit 'Command "wget" not found'
+type python > /dev/null 2>&1 || error_exit 'Command "python" not found'
+type samtools > /dev/null 2>&1 || error_exit 'Command "samtools" not found'
+type minimap2 > /dev/null 2>&1 || error_exit 'Command "minimap2" not found'
+
+python -c "import tensorflow as tf" > /dev/null 2>&1 ||
+error_exit '"Tensorflow" not found'
+
+#===========================================================
+#? For WSL (Windows Subsystem for Linux)
+#===========================================================
+
+uname -a |
+grep Microsoft 1>/dev/null 2>/dev/null &&
+alias python="python.exe"
+
+################################################################################
+#! Formatting environments
+################################################################################
+
+#===========================================================
+#? Make temporal directory
+#===========================================================
+rm -rf ".DAJIN_temp" 2>/dev/null || true
+dirs="fasta fasta_conv fasta_ont NanoSim bam igvjs data clustering/temp seqlogo/temp"
+
+echo "${dirs}" |
+    sed "s:^:.DAJIN_temp/:g" |
+    sed "s: : .DAJIN_temp/:g" |
+xargs mkdir -p
+
+#===========================================================
+#? Format FASTA file
+#===========================================================
+
+cat "${design}" |
+    tr -d "\r" |
+    awk '{if($1~"^>"){print "\n"$0}
+    else {printf $0}}
+    END {print ""}' |
+    grep -v "^$" |
+cat > .DAJIN_temp/fasta/fasta.fa
+
+design_LF=".DAJIN_temp/fasta/fasta.fa"
+
+# Separate multiple-FASTA into FASTA files
+cat ${design_LF} |
+    sed "s/^/@/g" |
+    tr -d "\n" |
+    sed -e "s/@>/\n>/g" \
+        -e "s/@/ /g" \
+        -e "s/$/\n/g" |
+    grep -v "^$" |
+    awk '{id=$1
+        gsub(">","",id)
+        output=".DAJIN_temp/fasta/"id".fa"
+        print $1"\n"toupper($2) > output
+    }'
+
+#===========================================================
+#? Reverse complement if the mutation sites are closer
+#? to right flanking than left flanking
+#===========================================================
+
+wt_seqlen=$(awk '!/[>|@]/ {print length($0)}' .DAJIN_temp/fasta/wt.fa)
+
+convert_revcomp=$(
+    minimap2 -ax splice \
+    .DAJIN_temp/fasta/wt.fa \
+    .DAJIN_temp/fasta/target.fa --cs 2>/dev/null |
+    awk '{for(i=1; i<=NF;i++) if($i ~ /cs:Z/) print $i}' |
+    sed -e "s/cs:Z:://g" -e "s/:/\t/g" -e "s/~/\t/g" |
+    tr -d "\~\*\-\+atgc" |
+    awk '{$NF=0; for(i=1;i<=NF;i++) sum+=$i} END{print $1,sum}' |
+    awk -v wt_seqlen="$wt_seqlen" \
+    '{if(wt_seqlen-$2>$1) print 0; else print 1}'
+    )
+
+if [ "$convert_revcomp" -eq 1 ] ; then
+    cat "${design_LF}" |
+        ./DAJIN/src/revcomp.sh - |
+    cat > .DAJIN_temp/fasta/fasta_revcomp.fa
+    design_LF=".DAJIN_temp/fasta/fasta_revcomp.fa"
+fi
+
+#---------------------------------------
+#* Separate multiple-FASTA into FASTA files
+#---------------------------------------
+cat ${design_LF} |
+    sed "s/^/@/g" |
+    tr -d "\n" |
+    sed -e "s/@>/\n>/g" \
+        -e "s/@/ /g" \
+        -e "s/$/\n/g" |
+    grep -v "^$" |
+    awk '{id=$1
+        gsub(">","",id)
+        output=".DAJIN_temp/fasta_conv/"id".fa"
+        print $1"\n"toupper($2) > output
+    }'
+
+#---------------------------------------
+#* Define mutation type
+#---------------------------------------
+mutation_type=$(
+    minimap2 -ax map-ont \
+        .DAJIN_temp/fasta/wt.fa \
+        .DAJIN_temp/fasta/target.fa \
+        --cs 2>/dev/null |
+    grep -v "^@" |
+    awk '{
+        cstag=$(NF-1)
+        if(cstag ~ "-") print "D"
+        else if(cstag ~ "\+") print "I"
+        else if(cstag ~ "\*") print "S"
+        }' 2>/dev/null
+)
+
+#---------------------------------------
+#* In the case of Point mutation:
+#* Generate randome insertion and deletion at gRNA sites as abnormal alleles
+#---------------------------------------
+
+if [ "_${mutation_type}" = "_S" ]; then
+    grna_len=$(awk -v grna="$grna" 'BEGIN{print length(grna)}')
+    grna_firsthalf=$(awk -v grna="$grna" 'BEGIN{print substr(grna, 1, int(length(grna)/2))}')
+    grna_secondhalf=$(awk -v grna="$grna" 'BEGIN{print substr(grna, int(length(grna)/2)+1, length(grna))}')
+    # Randome sequence
+    ins_seq=$(
+        seq_length="$grna_len" &&
+        od -A n  -t u4 -N $(($seq_length*100)) /dev/urandom |
+        tr -d "\n" |
+        sed 's/[^0-9]//g' |
+        sed "s/[4-9]//g" |
+        sed -e "s/0/A/g" -e "s/1/G/g" -e "s/2/C/g" -e "s/3/T/g" |
+        awk -v seq_length=$seq_length '{print substr($0, 1, seq_length)}'
+    )
+    # insertion
+    cat .DAJIN_temp/fasta_conv/wt.fa |
+        sed "s/$grna/$grna_firsthalf,$grna_secondhalf/g" |
+        sed "s/,/$ins_seq/g" |
+        sed "s/>wt/>wt_ins/g" |
+    cat > .DAJIN_temp/fasta_conv/wt_ins.fa
+    # deletion
+    cat .DAJIN_temp/fasta_conv/wt.fa |
+        sed "s/$grna//g" |
+        sed "s/>wt/>wt_del/g" |
+    cat > .DAJIN_temp/fasta_conv/wt_del.fa
+fi
+
+
+for input in ${ont_dir}/* ; do
+    output=$(
+        echo "${input}" |
+        sed -e "s#.*/#.DAJIN_temp/fasta_ont/#g" \
+            -e "s#\.f.*#.fa#g")
+    # Check wheather the files are binary:
+    if [ "$(file ${input} | grep -c compressed)" -eq 1 ]
+    then
+        gzip -dc "${input}"
+    else
+        cat "${input}"
+    fi |
+    awk '{if((4+NR)%4==1 || (4+NR)%4==2) print $0}' |
+    sed "s/^@/>/g" |
+    cat > "${output}"
+done
+
+################################################################################
+#! NanoSim (v2.5.0)
+################################################################################
+set +u
+conda activate DAJIN_nanosim
+set -u
+
+cat << EOF
+++++++++++++++++++++++++++++++++++++++++++
+NanoSim read simulation
+++++++++++++++++++++++++++++++++++++++++++
+EOF
+
+#===========================================================
+#? NanoSim
+#===========================================================
+
+printf "Read analysis...\n"
+./DAJIN/utils/NanoSim/src/read_analysis.py genome \
+    -i ".DAJIN_temp/fasta_ont/${ont_cont}.fa" \
+    -rg .DAJIN_temp/fasta_conv/wt.fa \
+    -t ${threads:-1} \
+    -o .DAJIN_temp/NanoSim/training
+
+wt_seqlen=$(awk '!/[>|@]/ {print length($0)}' .DAJIN_temp/fasta/wt.fa)
+
+for input in .DAJIN_temp/fasta_conv/*; do
+    printf "${input} is now simulating...\n"
+    output=$(
+        echo "$input" |
+        sed -e "s#fasta_conv/#fasta_ont/#g" \
+            -e "s/.fasta$//g" -e "s/.fa$//g"
+        )
+    ## For deletion allele
+    input_seqlength=$(
+        cat "${input}" |
+        awk '!/[>|@]/ {print length($0)-100}'
+        )
+    if [ "$input_seqlength" -lt "$wt_seqlen" ]; then
+        len=${input_seqlength}
+    else
+        len=${wt_seqlen}
+    fi
+    ##
+    ./DAJIN/utils/NanoSim/src/simulator.py genome \
+        -dna_type linear \
+        -c .DAJIN_temp/NanoSim/training \
+        -rg "${input}" \
+        -n 10000 \
+        -t "${threads:-1}" \
+        -min "${len}" \
+        -o "${output}_simulated"
+    ##
+    rm .DAJIN_temp/fasta_ont/*_error_* .DAJIN_temp/fasta_ont/*_unaligned_* 2>/dev/null || true
+done
+
+rm -rf DAJIN/utils/NanoSim/src/__pycache__
+
+printf 'Success!!\nSimulation is finished\n'
+
+################################################################################
+#! MIDS conversion
+################################################################################
+set +u
+conda activate DAJIN
+set -u
+
+cat << EOF
+++++++++++++++++++++++++++++++++++++++++++
+Converting ACGT into MIDS format
+++++++++++++++++++++++++++++++++++++++++++
+EOF
+
+reference=".DAJIN_temp/fasta_conv/wt.fa"
+query=".DAJIN_temp/fasta_conv/target.fa"
+
+#===========================================================
+#? Get mutation loci
+#===========================================================
+
+cat "${reference}" |
+    minimap2 -ax splice - "${query}" --cs 2>/dev/null |
+    awk '{for(i=1; i<=NF;i++) if($i ~ /cs:Z/) print $i}' |
+    sed -e "s/cs:Z:://g" -e "s/:/\t/g" -e "s/~/\t/g" |
+    tr -d "\~\*\-\+atgc" |
+    awk '{$NF=0; for(i=1;i<=NF;i++) sum+=$i} END{print $1,sum}' |
+cat > .DAJIN_temp/data/mutation_points
+
+#===========================================================
+#? MIDS conversion
+#===========================================================
+
+find .DAJIN_temp/fasta_ont -type f | sort |
+    awk '{print "./DAJIN/src/mids_classification.sh", $0, "wt", "&"}' |
+    awk -v th=${threads:-1} '{
+        if (NR%th==0) gsub("&","&\nwait",$0)
+        print}
+        END{print "wait"}' |
+sh - 2>/dev/null
+
+[ "_${mutation_type}" = "_S" ] && rm .DAJIN_temp/data/MIDS_target*
+
+printf "MIDS conversion was finished...\n"
+
+#===========================================================
+#? ACGT conversion
+#===========================================================
+
+find .DAJIN_temp/fasta_ont -type f | sort |
+    awk '{print "./DAJIN/src/acgt_classification.sh", $0, "wt", "&"}' |
+    awk -v th=${threads:-1} '{
+        if (NR%th==0) gsub("&","&\nwait",$0)
+        print}
+        END{print "wait"}' |
+sh - 2>/dev/null
+
+[ "_${mutation_type}" = "_S" ] && rm .DAJIN_temp/data/MIDS_target*
+
+printf "ACGT conversion was finished...\n"
+
+################################################################################
+#! Prediction
+################################################################################
+
+cat << EOF
+++++++++++++++++++++++++++++++++++++++++++
+Allele prediction
+++++++++++++++++++++++++++++++++++++++++++
+EOF
+
+
+#===========================================================
+#? Train models
+#===========================================================
+
+cat .DAJIN_temp/data/MIDS_* |
+    grep "_sim" |
+    grep -v "^ab_" |
+    sed -e "s/_aligned_reads//g" |
+cat > ".DAJIN_temp/data/DAJIN_MIDS_sim.txt"
+
+cat .DAJIN_temp/data/MIDS_"${ont_cont}"_wt |
+    grep -v "IIIIIIIIII" |
+    grep -v "DDDDDDDDDD" |
+    grep -v "SSSSSSSSSS" |
+    head -n 10000 |
+    sed "s/${ont_cont}$/wt_simulated/g" |
+cat >> ".DAJIN_temp/data/DAJIN_MIDS_sim.txt"
+
+python ./DAJIN/src/ml_simulated.py \
+    ".DAJIN_temp/data/DAJIN_MIDS_sim.txt" "${threads}"
+
+#===========================================================
+#? Predict labels
+#===========================================================
+
+true > ".DAJIN_temp/data/DAJIN_MIDS_prediction_result.txt"
+
+find .DAJIN_temp/data/MIDS* |
+    grep -v sim |
+    sort |
+while read -r input; do
+    barcode=$(echo $input | cut -d "_" -f 3)
+    echo "${barcode} is now processing..."
+
+    python ./DAJIN/src/ml_real.py \
+        "${input}" \
+        "${mutation_type}" "${threads}" ||
+    exit 1
+done
+
+cat .DAJIN_temp/data/DAJIN_MIDS_prediction_result.txt |
+    sort |
+cat > .DAJIN_temp/tmp_$$
+mv .DAJIN_temp/tmp_$$ .DAJIN_temp/data/DAJIN_MIDS_prediction_result.txt
+
+printf "Prediction was finished...\n"
+
+# #===========================================================
+# #? Report the percentage of alleles in each sample
+# #===========================================================
+
+#---------------------------------------
+#* Report the percentage of alleles in each sample
+#---------------------------------------
+
+cat .DAJIN_temp/data/DAJIN_MIDS_prediction_result.txt |
+    cut -f 2,3 |
+    sort |
+    uniq -c |
+    awk '{barcode[$2]+=$1
+        read_info[$2]=$1"____"$3" "read_info[$2]}
+    END{for(key in barcode) print key,barcode[key], read_info[key]}' |
+    awk '{for(i=3;i<=NF; i++) print $1,$2,$i}' |
+    sed "s/____/ /g" |
+    awk '{print $1, $3/$2*100, $4}' |
+cat > ".DAJIN_temp/tmp_prediction_proportion"
+
+
+exit 0
---
+++ src/acgt_classification.sh
@@ -0,0 +1,212 @@
+#!/bin/sh
+
+################################################################################
+#! Initialize shell environment
+################################################################################
+
+set -u
+umask 0022
+export LC_ALL=C
+type command >/dev/null 2>&1 && type getconf >/dev/null 2>&1 &&
+export UNIX_STD=2003  # to make HP-UX conform to POSIX
+
+################################################################################
+#! I/O naming
+################################################################################
+#===========================================================
+#? Auguments
+#===========================================================
+# input_fa=".DAJIN_temp/fasta_ont/wt_simulated_aligned_reads.fasta"
+# genotype="wt"
+# label=$(echo "${input_fa}" | sed -e "s#.*/##g" -e "s#\..*##g" -e "s/_aligned_reads//g")
+# suffix="${label}_${genotype}"
+
+input_fa=${1}
+genotype=${2}
+label=$(echo "${input_fa}" | sed -e "s#.*/##g" -e "s#\..*##g" -e "s/_aligned_reads//g")
+suffix="${label}_${genotype}"
+
+#===========================================================
+#? Input
+#===========================================================
+
+#===========================================================
+#? Output
+#===========================================================
+output_ACGT=".DAJIN_temp/data/ACGT_${suffix}"
+
+#===========================================================
+#? Temporal
+#===========================================================
+tmp_mapping=".DAJIN_temp/tmp_mapping_${suffix}"_$$
+tmp_seqID=".DAJIN_temp/tmp_seqID_${suffix}"_$$
+
+tmp_all=".DAJIN_temp/tmp_all_${suffix}"_$$
+tmp_primary=".DAJIN_temp/tmp_primary_${suffix}"_$$
+tmp_secondary=".DAJIN_temp/tmp_secondary_${suffix}"_$$
+
+################################################################################
+#! Function
+################################################################################
+# cat << EOF > test_1.sam
+# test primary minus 1 =G+a-c=A*ac
+# test primary minus 1 =AA+aa-aa=AA*ca*ca
+# EOF
+
+acgt_conv(){
+    set /dev/stdin
+    cat "${1}" |
+        awk '{id=$1; strand=$3; loc=$4; $0=$5
+        sub("cs:Z:","",$0)
+        gsub("-[acgt]*", "", $0)
+        gsub(/\*[acgt]/, "", $0)
+        gsub("=", "", $0)
+        gsub("\+", "", $0)
+        print id, loc, toupper($0)}' |
+    sort -t " " |
+    cat
+}
+
+################################################################################
+#! 変異部から±100塩基を含むリードのみを取り出す
+################################################################################
+
+#===========================================================
+#? 変数の定義
+# 切断面から含むべき塩基数
+#===========================================================
+
+reference=".DAJIN_temp/fasta_conv/wt.fa"
+reflength=$(cat "${reference}" | grep -v "^>" | awk '{print length($0)}')
+ref=$(cat "${reference}" | grep "^>" | sed "s/>//g")
+
+ext=${ext:=100}
+first_flank=$(
+    cat .DAJIN_temp/data/mutation_points |
+    awk -v ext=${ext} '{print $1-ext}'
+    )
+[ "${first_flank}" -lt 1 ] && first_flank=1
+
+second_flank=$(
+    cat .DAJIN_temp/data/mutation_points |
+    awk -v ext=${ext} '{if(NF==2) print $2+ext; else print $1+ext}'
+    )
+[ "${second_flank}" -gt "${reflength}" ] && second_flank="${reflength}"
+
+#===========================================================
+#? 変異部から±100塩基を含むリードのみを取り出す
+#===========================================================
+
+minimap2 -ax map-ont "${reference}" "${input_fa}" --cs=long 2>/dev/null |
+    awk -v ref="${ref}" -v reflen="${reflength}" '$3 == ref && length($10) < reflen * 1.1' |
+    tee "${tmp_mapping}" |
+    grep -v "^@" |
+    # fetch sequence start and end sites
+    awk 'BEGIN{OFS="\t"}{
+        cigar=$6;
+        gsub("[0-9]*S","",cigar);
+        gsub("[0-9]*H","",cigar);
+        gsub("M|D|I|N","\t",cigar);
+        gsub(/\+$/,"",cigar);
+        print $1, $4, cigar}' |
+    awk '{sum=0; for(i=3; i<=NF; i++){ sum+=$i }
+        print $1,$2,$2+sum}' |
+    sort -t " " -n |
+    awk '{if(length(min[$1])==0) min[$1]="inf";
+        if(min[$1]>$2) min[$1]=$2;
+        if(max[$1]<$3) max[$1]=$3}
+        END{for(key in min) print key, min[key], max[key]}' |
+    sort -t " " -n |
+    awk -v first=${first_flank} -v second=${second_flank} '{
+        if($2<=first && $3>=second) print $1}' |
+    sort |
+cat > "${tmp_seqID}"
+
+################################################################################
+#! MIDS conversion
+################################################################################
+
+#===========================================================
+#? primaryとsecondaryを分けてMIDS変換を行う
+#===========================================================
+
+cat "${tmp_mapping}" |
+    sort |
+    join - "${tmp_seqID}" |
+    # append alignment info
+    awk '{
+        if($2==0 || $2==16) {alignment="primary"} else {alignment="secondary"};
+        if($2==0 || $2==2048) {strand="plus"} else {strand="minus"};
+        for(i=1;i<=NF;i++) if($i ~ /cs:Z/) print $1,alignment,strand,$4,$i
+    }' |
+cat > "${tmp_all}"
+
+cat "${tmp_all}" |
+    grep "primary" |
+    acgt_conv |
+cat > "${tmp_primary}"
+
+cat "${tmp_all}" |
+    grep "secondary" |
+    acgt_conv |
+cat > "${tmp_secondary}"
+
+#===========================================================
+#? primaryとsecondaryを結合する
+#===========================================================
+
+cat "${tmp_primary}" "${tmp_secondary}" |
+    sort -t " " -k 2,2n |
+    awk '{seq_[$1]=seq_[$1]" "$2" "$3" "$4}
+        END{for(key in seq_) print key,seq_[key]}' |
+    # grep inversion_100_aligned_7213_F_54_2709_73 |
+    awk 'NF==3
+    #---------------------------------------
+    #* Flox deletion ("=")
+    #---------------------------------------
+    NF==5{id=$1; minloc=$2; maxloc=$2 ; seq=""; len=0; str=""
+        for(i=2; i<=NF; i=i+2){
+            seq_[$i]=$(i+1)
+            if(minloc>$i){ minloc=$i; len=length($(i+1))}
+            if(maxloc<$i) maxloc=$i
+        }
+        len=maxloc-minloc-len
+        for(len_=1; len_ <= len; len_++) str = "=" str
+        for(key in seq_) seq=seq seq_[key] str
+        print id, minloc, seq
+        delete seq_
+    }
+    #---------------------------------------
+    #* Inversion ("=")
+    #---------------------------------------
+    NF==7{id=$1; minloc=$2; maxloc=$2 ; seq=""; len=0; str=""
+        for(i=2; i<=NF; i=i+2){
+            if(minloc>$i){ minloc=$i; len=length($(i+1))}
+            if(maxloc<$i) maxloc=$i
+        }
+        len=length($5)
+        str=sprintf("%"len"s","")
+        gsub(/ /,"=",str)
+        $5=str
+        print id, minloc, $3 $5 $7
+    }' |
+    sed "s/D*$//g" |
+    #---------------------------------------
+    #* Complement seqences to match sequence length (insert "=")
+    #---------------------------------------
+    ## start
+    awk '{start=""; for(i=1; i < $2; i++) start=start"M"; print $1,$2,start""$3}' |
+    ## end
+    awk -v reflen="${reflength}" '{
+        seqlen=length($3);
+        end="";
+        if(seqlen>reflen) {print $1,substr($3,1,reflen)}
+        else {for(i=seqlen; i < reflen; i++) end=end"="; print $1,$3""end}
+    }' |
+    # 全てが変異になったリードがあれば除去する。
+    awk '$2 !~ /^[I|D|S|=]+$/' |
+    sed -e "s/$/\t${label}/g" -e "s/ /\t/g" |
+cat > "${output_ACGT}"
+
+rm "${tmp_mapping}" "${tmp_seqID}" "${tmp_all}" "${tmp_primary}" "${tmp_secondary}"
+exit 0
