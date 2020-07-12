e9e001d9c6cbc04bed87dc1966e4ebd8282c7798
akikuno <akikuno@users.noreply.github.com>
2020-07-12 14:33:11 +0900

    add: jitterplot

--- src/ml_accuracy_ggplot2.R
+++ src/ml_accuracy_ggplot2.R
@@ -1,15 +1,20 @@
 library(tidyverse)
 
 df <- read_csv("accuracy_anomaly_detection.csv",
-    col_names=c("id","value","L2", "MIDS"))
+    col_names=c("id","Accuracy","L2", "MIDS"),
+    col_types=cols())
 
 df$id <- df$id %>%
     str_replace("abtest_wt_","") %>%
     str_replace("_simulated","")
 
-p <- ggplot(df, aes(x=id, y=value)) +
-    geom_boxplot() +
-    # geom_point() +
+df <- df %>% filter(!str_detect(id, "ins"))
+
+p <- ggplot(df, aes(x=id, y=Accuracy)) +
+    geom_boxplot(outlier.shape = NA) +
+    geom_jitter(width=0.2, height=0.05, alpha=0.35, size=0.75) +
+    theme_bw() +
     facet_wrap(~ L2 + MIDS)
 
 ggsave("accuracy_anomaly_detection.png", p, width = 12)
+ggsave("accuracy_anomaly_detection.pdf", p, width = 12)
