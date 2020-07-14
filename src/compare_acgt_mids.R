393abcaec356248015bf268d50b5e21dfdedadea
akikuno <akikuno@users.noreply.github.com>
2020-07-14 12:28:08 +0900

    update: factor before after

--- src/compare_acgt_mids.R
+++ src/compare_acgt_mids.R
@@ -9,12 +9,14 @@ df$id <- df$id %>%
     str_replace("_simulated","") %>%
     str_replace("nega","")
 
+df$bfaf <- factor(df$bfaf, level=c("before","after"))
+
 df <- df %>% filter(!str_detect(id, "ins"))
 
 p <- ggplot(df, aes(x=umap1, y=umap2, fill=id)) +
     geom_point(aes(color = id, shape = id, alpha = 0.5)) +
     theme_bw() +
-    facet_wrap(~ conv + bfaf)
+    facet_wrap(~ bfaf + conv, nrow = 1)
 
-ggsave("umap_compare_acgt_mids.png", p)
-ggsave("umap_compare_acgt_mids.pdf", p)
+ggsave("umap_compare_acgt_mids.png", p, width = 16, height = 4)
+ggsave("umap_compare_acgt_mids.pdf", p, width = 16, height = 4)
