b35cce5bb8c7b092eea5add6acacf91980cf32fd
akikuno <akikuno@users.noreply.github.com>
2020-07-28 16:15:23 +0900

    rename: control_score.sh

--- src/clustering_control_score.sh
+++ src/clustering_control_score.sh
@@ -7,7 +7,6 @@
 set -eu
 umask 0022
 export LC_ALL=C
-type command >/dev/null 2>&1 && type getconf >/dev/null 2>&1 &&
 export UNIX_STD=2003  # to make HP-UX conform to POSIX
 
 ################################################################################
@@ -19,40 +18,34 @@ export UNIX_STD=2003  # to make HP-UX conform to POSIX
 #===========================================================
 
 # barcode=barcode34
-# alleletype=flox_deletion
 # threads=12
-# mapping_alleletype="${alleletype}"
-# [ "$alleletype" = "normal" ] && mapping_alleletype="wt"
-# [ "$alleletype" = "abnormal" ] && mapping_alleletype="wt"
 
 #===========================================================
 #? Auguments
 #===========================================================
 
 barcode="${1}"
-alleletype="${2}"
 threads="${3}"
-mapping_alleletype="${alleletype}"
-[ "$alleletype" = "normal" ] && mapping_alleletype="wt"
-[ "$alleletype" = "abnormal" ] && mapping_alleletype="wt"
 
 #===========================================================
 #? Input
 #===========================================================
 
+alleletype="wt"
+
 #===========================================================
 #? Output
 #===========================================================
 mkdir -p ".DAJIN_temp/clustering/temp/"
-control_score=".DAJIN_temp/clustering/temp/control_score_${mapping_alleletype}"
+control_score=".DAJIN_temp/clustering/temp/control_score_${alleletype}"
 
 #===========================================================
 #? Temporal
 #===========================================================
 MIDS_ref=".DAJIN_temp/clustering/temp/MIDS_${barcode}_${alleletype}"
-MIDS_tmp=".DAJIN_temp/clustering/temp/MIDS_tmp_${barcode}_${alleletype}"
-control_tmp=".DAJIN_temp/clustering/temp/control_tmp_${barcode}_${alleletype}"
-label_nuc_tmp=".DAJIN_temp/clustering/temp/label_nuc_tmp_${barcode}_${alleletype}"
+tmp_MIDS=".DAJIN_temp/clustering/temp/tmp_MIDS_${barcode}_${alleletype}"
+tmp_control=".DAJIN_temp/clustering/temp/tmp_control_${barcode}_${alleletype}"
+tmp_label_nuc=".DAJIN_temp/clustering/temp/tmp_label_nuc_${barcode}_${alleletype}"
 
 ################################################################################
 #! Control scoring
@@ -62,7 +55,7 @@ label_nuc_tmp=".DAJIN_temp/clustering/temp/label_nuc_tmp_${barcode}_${alleletype
 #? MIDS conversion
 #===========================================================
 
-./DAJIN/src/mids_clustering.sh "${barcode}" "${mapping_alleletype}" > "${MIDS_ref}"
+./DAJIN/src/mids_clustering.sh "${barcode}" "${alleletype}" > "${MIDS_ref}"
 
 #===========================================================
 #? Mutation scoring of control samples
@@ -74,22 +67,20 @@ cat "${MIDS_ref}" |
     join - .DAJIN_temp/data/DAJIN_MIDS_prediction_result.txt |
     awk -v alelle="$alleletype" '$NF==alelle' |
     cut -d " " -f 2 |
-cat > "${MIDS_tmp}"
+cat > "${tmp_MIDS}"
 
 # ----------------------------------------
 # Transpose matrix
 # ----------------------------------------
-nr=$(cat "${MIDS_tmp}" | wc -l)
+nr=$(cat "${tmp_MIDS}" | wc -l)
 
-cat "${MIDS_tmp}" |
+cat "${tmp_MIDS}" |
     awk -v nr="${nr}" -F "" \
     '{for(i=1; i<=NF; i++){
             row[i]=row[i] $i
             if(NR==nr) print row[i]
         }
     }' |
-    # cat tmp | head -n 3113 | tail -n 1 |
-    # head -n 3208 tmp | tail |
     awk -F "" '{
         INS=gsub(/[1-9]|[a-z]/,"@",$0)
         DEL=gsub("D","D",$0)
@@ -98,9 +89,11 @@ cat "${MIDS_tmp}" |
         #* Define sequence error when control sample has more than 5% mutations
         # ----------------------------------------
         per=5
-        # print INS, DEL, SUB, NF*per/100 #!<<<<
-        if(INS > NF*per/100 || DEL > NF*per/100 || SUB > NF*per/100) num = 2
-        else num=1
+        if(INS > NF*per/100 || DEL > NF*per/100 || SUB > NF*per/100)
+            num=2
+        else
+            num=1
+
         print num
     }' |
 cat > "${control_score}"
@@ -113,9 +106,9 @@ cat > "${control_score}"
 cat "${control_score}" |
     awk '{print $1,NR}' |
     sort -t " " -k 2,2 |
-cat > "${control_tmp}"
+cat > "${tmp_control}"
 
-find .DAJIN_temp/fasta/* |
+find .DAJIN_temp/fasta/ -type f |
     grep -v wt.fa |
     grep -v fasta.fa |
     sed "s:.*/::g" |
@@ -130,24 +123,24 @@ while read -r label; do
         sed "s/=//g" |
         sort -t " " -k 1,1n |
         cut -d " " -f 2 |
-    cat > "${label_nuc_tmp}"
+    cat > "${tmp_label_nuc}"
     #
-    if [ "$(cat "${label_nuc_tmp}" | wc -l)" -eq 3 ]; then # inversion
-        cat "${label_nuc_tmp}" |
+    if [ "$(cat "${tmp_label_nuc}" | wc -l)" -eq 3 ]; then # inversion
+        cat "${tmp_label_nuc}" |
             awk 'NR==2{printf tolower($1); next} {printf $1}'
-    elif [ "$(cat "${label_nuc_tmp}" | wc -l)" -eq 2 ]; then # flox deletion
+    elif [ "$(cat "${tmp_label_nuc}" | wc -l)" -eq 2 ]; then # flox deletion
         wt_length=$(sed 1d .DAJIN_temp/fasta/wt.fa | awk '{print length}')
         que_length=$(sed 1d .DAJIN_temp/fasta/"${label}".fa | awk '{print length}')
         del_length=$((${wt_length}-${que_length}+1))
 
-        cat "${label_nuc_tmp}" |
+        cat "${tmp_label_nuc}" |
             awk -v del_len="${del_length}" 'NR==2{
                 seq=""
                 for(i=1;i<del_len;i++){seq=seq "a"}
                 $0=substr($0,2)
                 printf seq, $0}{printf $0}'
     else
-        cat "${label_nuc_tmp}"
+        cat "${tmp_label_nuc}"
     fi |
     sed "s/*[acgt]//g" |
     sed "s/[=+]//g" |
@@ -173,10 +166,10 @@ while read -r label; do
             }
         }}' |
     sort -t " " -k 2,2 |
-    join -a 1 -1 2 -2 2 - "${control_tmp}" |
+    join -a 1 -1 2 -2 2 - "${tmp_control}" |
     awk 'NF==3{$4=1}{print $0}' |
     sort -k 3,3n |
-    if [ "$(cat "${label_nuc_tmp}" | wc -l)" -eq 2 ]; then # flox deletion
+    if [ "$(cat "${tmp_label_nuc}" | wc -l)" -eq 2 ]; then # flox deletion
         cat - | grep -v "^NA"
     else
         cat -
@@ -184,6 +177,6 @@ while read -r label; do
     cat > ".DAJIN_temp/clustering/temp/control_score_${label}"
 done
 
-rm "${MIDS_ref}" "${MIDS_tmp}" "${control_tmp}" "${label_nuc_tmp}"
+rm "${MIDS_ref}" "${tmp_MIDS}" "${tmp_control}" "${tmp_label_nuc}"
 
 exit 0
