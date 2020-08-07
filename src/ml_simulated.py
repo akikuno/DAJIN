e8efcf75906a4d73183040b834cdef7d7aa60c78
akikuno <akikuno@users.noreply.github.com>
2020-08-07 20:47:54 +0900

    GPU listriction

--- src/ml_simulated.py
+++ src/ml_simulated.py
@@ -1,7 +1,7 @@
 import os
 
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-# os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
+os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
 
 import sys
 import numpy as np
