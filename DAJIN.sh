c1c443e9d8f45b981d4060e2ed1554bc7ac414e5
akikuno <akikuno@users.noreply.github.com>
2020-08-17 19:57:54 +0900

    add `generate_bam.sh`

--- DAJIN.sh
+++ DAJIN.sh
@@ -551,6 +551,7 @@ EOF
 #===========================================================
 #? Setting directory
 #===========================================================
+
 rm -rf .DAJIN_temp/consensus/ 2>/dev/null
 mkdir -p .DAJIN_temp/consensus/temp
 
@@ -585,87 +586,7 @@ Generate BAM files
 
 EOF
 
-#===========================================================
-#? Setting directory
-#===========================================================
-
-rm -rf .DAJIN_temp/bam/ 2>/dev/null
-mkdir -p .DAJIN_temp/bam/temp .DAJIN_temp/bam/reads20
-
-#===========================================================
-#? Generate BAM files
-#===========================================================
-
-if [ "_$mutation_type" = "_S" ]; then
-    mv .DAJIN_temp/fasta_ont/wt_ins* .DAJIN_temp/
-    mv .DAJIN_temp/fasta_ont/wt_del* .DAJIN_temp/
-fi
-
-./DAJIN/src/mapping.sh "${genome:-mm10}" "${threads:-1}" || exit 1
-
-if [ "_$mutation_type" = "_S" ]; then
-    mv .DAJIN_temp/wt_ins* .DAJIN_temp/fasta_ont/
-    mv .DAJIN_temp/wt_del* .DAJIN_temp/fasta_ont/
-fi
-
-#===========================================================
-#? Generate BAM files on each cluster
-#===========================================================
-
-cat .DAJIN_temp/clustering/label* |
-    awk '{nr[$1]++; print $0, nr[$1]}' |
-while read -r allele
-do
-    barcode=$(echo ${allele} | cut -d " " -f 1)
-    alleletype=$(echo ${allele} | cut -d " " -f 2)
-    cluster=$(echo ${allele} | cut -d " " -f 3)
-    alleleid=$(echo ${allele} | cut -d " " -f 5)
-    #
-    input_bam="${barcode}_${alleletype}"
-    output_bam="${barcode}_allele${alleleid}"
-    #
-    find .DAJIN_temp/clustering/readid_cl_mids* |
-        grep "${input_bam}" |
-        xargs cat |
-        awk -v cl="${cluster}" '$2==cl' |
-        cut -f 1 |
-        sort |
-    cat > ".DAJIN_temp/bam/temp/tmp_id_$$"
-
-    samtools view -h ".DAJIN_temp/bam/${barcode}".bam |
-        awk '/^@/{print}
-            NR==FNR{a[$1];next}
-            $1 in a' \
-            .DAJIN_temp/bam/temp/tmp_id_$$ - |
-        samtools sort -@ "${threads:-1}" 2>/dev/null |
-    cat > .DAJIN_temp/bam/"${output_bam}".bam
-    samtools index .DAJIN_temp/bam/"${output_bam}".bam
-
-done
-
-#===========================================================
-#? Generate BAM files with 20 reads
-#===========================================================
-
-find .DAJIN_temp/bam/ -name "*bam" -type f |
-grep -v "reads20" |
-while read -r input_bam; do
-
-    output_bam=$(
-        echo "${input_bam}" |
-        sed "s%.DAJIN_temp/bam/%.DAJIN_temp/bam/reads20/%g"
-        )
-
-    header_num=$(samtools view -H "${input_bam}" | wc -l)
-    bam_num=$((20 + "${header_num}"))
-
-    samtools view -h "${input_bam}" |
-        awk '$1 ~ /^@/ || $6 != "*"' |
-        head -n "${bam_num}" |
-        samtools sort -@ "${threads:-1}" 2>/dev/null |
-    cat > "${output_bam}"
-    samtools index "${output_bam}"
-done
+./DAJIN/src/generate_bam.sh "${genome}" "${threads}"
 
 ################################################################################
 #! Move output files
