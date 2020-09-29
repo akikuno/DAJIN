359187ce1ee1fca5568bc0843bae5eb4f992756a
akikuno <akikuno@users.noreply.github.com>
2020-09-30 06:47:29 +0900

    omit code to remove temporal file

--- src/clustering.sh
+++ src/clustering.sh
@@ -111,10 +111,5 @@ echo "Clustering ${barcode} ${alleletype}..." >&2
 Rscript DAJIN/src/clustering.R "${query_score}" "${query_label}" "${control_score}" "${threads}" 2>/dev/null || true
 ps -au | grep -e "clustering.R" -e "joblib" | awk '{print $2}'| xargs kill 2>/dev/null || true
 
-################################################################################
-#! Clean and Finish
-################################################################################
-
-rm "${MIDS_que}" "${query_score}" "${query_label}"
 
 exit 0
