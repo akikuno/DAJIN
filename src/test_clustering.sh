67e0bc92b9f00f0fa6b5cd768679f236bb941517
akikuno <akikuno@users.noreply.github.com>
2020-10-20 21:47:07 +0900

    add || true

--- src/test_clustering.sh
+++ src/test_clustering.sh
@@ -120,8 +120,8 @@ cat > "${query_label}"
 
 echo "Clustering ${barcode} ${alleletype}..." >&2
 if [ "$(cat ${query_label} | wc -l)" -gt 50 ]; then
-    Rscript DAJIN/src/test_clustering.R "${query_score}" "${query_label}" "${control_RDS}" "${threads}" 2>&1 | grep -v "joblib" #2>/dev/null || echo "TERMINATED" #! RENAME =================
+    Rscript DAJIN/src/test_clustering.R "${query_score}" "${query_label}" "${control_RDS}" "${threads}" 2>&1 | grep -v "joblib" || true #2>/dev/null || echo "TERMINATED" #! RENAME =================
     ps -au | grep -e "clustering.R" -e "joblib" | awk '{print $2}'| xargs kill 2>/dev/null || true
 fi
 
-echo "finished ${barcode} ${alleletype}..." >&2
+echo "finished ${barcode} ${alleletype}..."
