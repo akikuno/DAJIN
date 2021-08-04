d72dc8d1bc6ce5f8402eee69f277547b3ac407a3
Akihiro Kuno <akikuno@users.noreply.github.com>
2021-08-04 13:18:51 +0900

    work with tensorflow-gpu in RTX

--- src/conda_setting.sh
+++ src/conda_setting.sh
@@ -7,24 +7,25 @@ error_exit() {
     exit 1
 }
 
+CONDA_BASE=$(conda info --base)
+. "${CONDA_BASE}/etc/profile.d/conda.sh"
 conda --version >/dev/null 2>&1 || error_exit 'Command "conda" not found'
+conda update -y -n base conda >/dev/null 2>&1
+conda config --add channels defaults 2>/dev/null
+conda config --add channels bioconda 2>/dev/null
+conda config --add channels conda-forge 2>/dev/null
+
+conda list | grep -q mamba || conda install -y -c conda-forge mamba >/dev/null 2>&1
 
 #===========================================================
 #? DAJIN_nanosim
 #===========================================================
 
-CONDA_BASE=$(conda info --base)
-. "${CONDA_BASE}/etc/profile.d/conda.sh"
-conda config --add channels defaults 2>/dev/null
-conda config --add channels bioconda 2>/dev/null
-conda config --add channels conda-forge 2>/dev/null
-
 if [ "$(conda info -e | grep -c DAJIN_nanosim)" -eq 0 ]; then
     echo Create "DAJIN_nanosim" environment... >&2
-    conda update -y conda >/dev/null 2>&1
     conda create -y -n DAJIN_nanosim python=3.6 >/dev/null 2>&1
-    conda install -y -n DAJIN_nanosim --file ./DAJIN/utils/NanoSim/requirements.txt >/dev/null 2>&1
-    conda install -y -n DAJIN_nanosim minimap2 >/dev/null 2>&1
+    mamba install -y -n DAJIN_nanosim --file ./DAJIN/utils/NanoSim/requirements.txt >/dev/null 2>&1
+    mamba install -y -n DAJIN_nanosim minimap2 >/dev/null 2>&1
 fi
 
 conda activate DAJIN_nanosim
@@ -48,19 +49,30 @@ conda deactivate
 
 if [ "$(conda info -e | cut -d " " -f 1 | grep -c DAJIN$)" -eq 0 ]; then
     echo Create "DAJIN" environment... >&2
-    conda update -y conda >/dev/null 2>&1
-    conda create -y -n DAJIN python=3.7 \
+    conda create -y -n DAJIN python=3.8 >/dev/null 2>&1
+    mamba install -y -n DAJIN \
         numpy pandas scikit-learn joblib hdbscan \
-        wget emboss samtools minimap2 \
-        r-essentials r-base r-pacman r-reticulate r-vroom r-furrr >/dev/null 2>&1
-    conda install -y -n DAJIN -c anaconda tensorflow tensorflow-gpu >/dev/null 2>&1
+        wget emboss samtools minimap2 >/dev/null 2>&1
+    mamba install -y -n DAJIN -c conda-forge r-essentials r-base r-reticulate r-vroom r-furrr >/dev/null 2>&1
+    # tensorflow setting (CPU, GPU w/ RTX, and GPU w/ GTX)
+    conda activate DAJIN
+    if ! type nvidia-smi >/dev/null 2>&1; then
+        mamba install -y -n DAJIN -c anaconda tensorflow >/dev/null 2>&1
+    elif nvidia-smi -q | grep -q RTX; then
+        mamba install -y -n DAJIN cudnn=8.0 cudatoolkit=11.0 >/dev/null 2>&1
+        pip install -U tensorflow-gpu==2.4.0 >/dev/null 2>&1
+    else
+        mamba install -y -n DAJIN -c anaconda tensorflow tensorflow-gpu >/dev/null 2>&1
+    fi
 fi
+
+conda activate DAJIN
+
+Rscript -e 'install.packages(c("pacman", "tidyfast"), repos="https://cloud.r-project.org/")' >/dev/null 2>&1
 
 #===========================================================
 #? Required software
 #===========================================================
-
-conda activate DAJIN
 
 gzip --version >/dev/null 2>&1 || error_exit 'Command "gzip" installation has failed'
 wget --version >/dev/null 2>&1 || error_exit 'Command "wget" installation has failed'
